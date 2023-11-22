f1cf0ef48f8b644a8602562eb2338ef7b5cfc886
wendel guilherme <wendel.guilherme@estudante.ufjf.br>
2023-11-21 22:46:37 -0300

    adicionado o for na funcao

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
+import { getLista } from "./src/lista.js";
+
+
 const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
 const olItens = document.querySelector("#itens");
 
-adicionaElementoNalista("Primeiro");
-adicionaElementoNalista("Segundo");
-adicionaElementoNalista("Terceiro");
+atualizarListaOrdenada();
 
-function adicionaElementoNalista(texto){
+function atualizarListaOrdenada(){
+    const lista = getLista();
+    for (let i = 0; i < lista.length; i++){
+    adicionaElementoNalistaOrdenada(lista[i]);
+}
+}
+
+
+function adicionaElementoNalistaOrdenada(texto){
     const li = document.createElement("li");
     li.textContent = texto;
     olItens.appendChild(li);
