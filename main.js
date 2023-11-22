27a84933567a3450cd4bbce79ea85c9aefc6eacf
wendel guilherme <wendel.guilherme@estudante.ufjf.br>
2023-11-21 22:19:28 -0300

    cria 3 li

--- main.js
+++ main.js
@@ -7,3 +7,11 @@ const olItens = document.querySelector("#itens");
 const li = document.createElement("li");
 li.textContent = "Primeiro";
 olItens.appendChild(li);
+
+const li2 = document.createElement("li");
+li2.textContent = "Segundo";
+olItens.appendChild(li2);
+
+const li3 = document.createElement("li");
+li3.textContent = "Terceiro";
+olItens.appendChild(li3);
