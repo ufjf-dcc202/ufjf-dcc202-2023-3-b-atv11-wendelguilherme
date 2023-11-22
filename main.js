aee6d2f99428ff9639da495415328a1549d9c627
wendel guilherme <wendel.guilherme@estudante.ufjf.br>
2023-11-21 22:22:21 -0300

    funcao adiciona elementos

--- main.js
+++ main.js
@@ -3,15 +3,14 @@ const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
 const olItens = document.querySelector("#itens");
 
+adicionaElementoNalista("Primeiro");
+adicionaElementoNalista("Segundo");
+adicionaElementoNalista("Terceiro");
 
-const li = document.createElement("li");
-li.textContent = "Primeiro";
-olItens.appendChild(li);
+function adicionaElementoNalista(texto){
+    const li = document.createElement("li");
+    li.textContent = texto;
+    olItens.appendChild(li);
+}
 
-const li2 = document.createElement("li");
-li2.textContent = "Segundo";
-olItens.appendChild(li2);
 
-const li3 = document.createElement("li");
-li3.textContent = "Terceiro";
-olItens.appendChild(li3);
